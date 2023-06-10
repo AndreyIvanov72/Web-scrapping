23844f33fed13d1b689dc43f3b2584ace6a1b723
AndreyIvanov72 <130745347+AndreyIvanov72@users.noreply.github.com>
2023-06-10 17:41:44 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+import requests
+from fake_headers import Headers
+from bs4 import BeautifulSoup
+import lxml
+
+
+url = 'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2'
+words = ['python', 'django', 'flask']
+
+def get_headers():
+    return Headers(browser='opera', os='win').generate()
+
+job_html = requests.get(url, headers=get_headers()).text
+
+job_soup = BeautifulSoup(job_html, 'lxml')
+
+articles_list = job_soup.find_all(class_='vacancy-serp-item__layout')
+
+
+def sample(date):
+    job_data = []
+    for article in articles_list:
+        link = article.find('a')['href']
+        try:
+            salary = article.find('span', {'data-qa':'vacancy-serp__vacancy-compensation'}).text
+        except:
+            salary = 'Нет данных!'
+        company = article.find('a', class_='bloko-link bloko-link_kind-tertiary').text
+        city = article.find('div',{'data-qa':'vacancy-serp__vacancy-address'}).text.split(',')[0]
+        job_data.append({
+            'Город': city,
+            'Компания': company,
+            'Зарплата': salary,
+            'Cсылка': link
+             })
+    return job_data
+
+def write(file):
+    with open('jobs.json', 'w', encoding='utf-8') as f:
+        data = json.dump(file, f, ensure_ascii=False, indent=2)
+        return data
+
+if __name__ == '__main__':
+    art = sample(articles_list)
+    write(art)
